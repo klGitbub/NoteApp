d41243f1827b9fd52cc1050d2a72031a3fb3d779
klGitbub <klaghaeian@gmail.com>
2023-03-13 17:26:04 -0400

    "Initial commit"

---
+++ noteApp/src/main/java/com/devmountain/noteApp/repositories/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.devmountain.noteApp.repositories;
+
+import com.devmountain.noteApp.entities.Note;
+import com.devmountain.noteApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findAllByUserEquals(User user);
+}
